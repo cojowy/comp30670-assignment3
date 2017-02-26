d8fbbf2eef8d43a8c2468424173f9c923fd94232
cojowy <16202597@ucdconnect.ie>
2017-02-26 10:58:01 +0000

    LED_board class created, main method defined

--- Conor/main.py
+++ Conor/main.py
@@ -1,6 +1,14 @@
 import sys
 import urllib.request
 
+class LED_board():
+    '''Class defined for the LED board'''
+    def __init__(self, L):
+        '''Board is initialized with an LxL array, with every value set to 
+        -1. This is the value used to indicate a light is off'''
+        self.array=[[-1]*L for _ in range(L)]
+        self.size=L
+     
 def read_file(link):
     '''Function which reads in a file from a URL and returns the
     contents of the file in a string'''
@@ -8,10 +16,8 @@ def read_file(link):
     buffer=req.read().decode('utf-8')   
     return buffer
 
-'''
-link=sys.argv[2] #Reads in the link from the command line parameter
-file=read_file(link)
-
-for line in file.split("\n"): #printing out file in the command line
-    print(line)
-'''
+def main():
+    link=sys.argv[2] #Reading link to file from command line parameter
+    file=read_file(link)
+    arraySize=int(file.split("\n")[0]) #Obtaining size of array from first line of file
+    board=LED_board(arraySize)
